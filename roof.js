b64f57f48934e703e947913502a27553ec82203b
saggneicZ <72294761+saggneicZ@users.noreply.github.com>
2020-11-20 23:37:23 +0530

    Add files via upload

---
+++ roof.js
@@ -0,0 +1,24 @@
+class roof
+{
+    constructor(x,y,w,h)
+    {
+        var options={
+            isStatic:true
+        }
+
+        this.x=x;
+        this.y=y;
+        this.w=w;
+        this.h=h;
+        this.body=Bodies.rectangle(x,y,w,h,options);
+        World.add(world,this.body);
+
+    }
+    display()
+    {
+        var groundPos=this.body.position;
+ 
+    }
+
+    push()
+}
